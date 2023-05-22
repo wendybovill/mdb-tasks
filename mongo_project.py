01a2487673b73dc6dcb1f31d5da3f45703521a52
Wendy Bovill <wendybovill@gmail.com>
2023-05-22 18:33:18 +0100

    Initial commit

---
+++ mongo_project.py
@@ -0,0 +1,171 @@
+import os
+import pymongo
+import pathlib
+from os import path
+if os.path.exists("env.py"):
+    import env
+
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "myFirstDB"
+COLLECTION = "celebreties"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+def show_menu():
+    print(" ")
+    print("1. Add a record")
+    print("2. Find a record by name")
+    print("3. Edit a record")
+    print("4. Delete a record")
+    print("5. Exit")
+
+    option = input("Enter option:")
+    return option
+
+
+def get_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+
+    try:
+        doc = coll.find_one({"first": first.lower(), "last": last.lower()})
+        print("")
+    except:
+        print("Record not found")
+
+    if not doc:
+        print("")
+        print("Error! No results found.")
+
+    return doc
+
+
+def get_records():
+    print("")
+
+    try:
+        doc = coll.find()
+        print("")
+    except:
+        print("No records found")
+
+    if not doc:
+        print("")
+        print("Error! No results found.")
+
+    return doc
+
+
+def find_record():
+    doc = get_record()
+    if doc:
+        print("")
+        for k,v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+
+def view_records():
+    doc = get_records()
+    if doc:
+        print("")
+        for k,v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+
+def edit_record():
+    doc = get_record()
+    if doc:
+        update_doc = {}
+        print("")
+        for k,v in doc.items():
+            if k != "_id":
+                update_doc[k] = input(k.capitalize() + " [" + v + "]  > ")
+
+                if update_doc[k] == "":
+                    update_doc[k] = v
+        try:
+            coll.update_one(doc, {"$set": update_doc})
+            print("")
+            print("Document Updated")
+        except:
+            print("Error accessing the database")
+
+
+def delete_record():
+    doc = get_record()
+    if doc:
+        print("")
+        for k,v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+        print("")
+        confirmation = input("Is this the record you want to delete?\nY or N > ")
+        print("")
+
+        if confirmation.lower() == "y":
+            try:
+                coll.delete_one(doc)
+                print("Record deleted")
+            except:
+                print("Error accessing the database")
+
+        else:
+            print("Document not deleted")
+
+def add_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+    dob = input("Enter date of birth > ")
+    gender = input("Enter gender > ")
+    hair_color = input("Enter hair color > ")
+    occupation = input("Enter occupation > ")
+    nationality = input("Enter nationality > ")
+
+    new_doc = {
+        "first": first.lower(),
+        "last": last.lower(),
+        "dob": dob.lower(),
+        "gender": gender.lower(),
+        "hair_color": hair_color.lower(),
+        "occupation": occupation.lower(),
+        "nationality": nationality.lower()
+    }
+
+    try:
+        coll.insert_one(new_doc)
+        print("")
+        print("Document Inserted")
+    except:
+        print("Error accessing the database")
+
+def main_loop():
+    while True:
+        option = show_menu()
+        if option == "1":
+            add_record()
+        elif option == "2":
+            find_record()
+        elif option == "3":
+            edit_record()
+        elif option == "4":
+            delete_record()
+        elif option == "5":
+            conn.close()
+            break
+        else:
+            print("Invalid option")
+        print("")
+
+conn = mongo_connect(MONGO_URI)
+coll = conn[DATABASE][COLLECTION]
+
+main_loop()
