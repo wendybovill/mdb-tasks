c29f1f698af7cd9700bb13506478fd8cfd9b84a1
wendybovill <wendybovill@gmail.com>
2023-05-23 19:13:07 +0000

    added add task functionality with datepicker and urgent option

--- static/js/script.js
+++ static/js/script.js
@@ -4,19 +4,19 @@
 
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
-});
-
-
-$(document).ready(function(){
     $('.collapsible').collapsible();
-  });
-
-  
-  $(document).ready(function(){
     $('.tooltipped').tooltip();
-  });
+    $('.scrollspy').scrollSpy();
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
+});
         
-
 
 /*
     vanilla JavaScript for MaterializeCSS initialization
